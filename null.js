a26fc3e2e4962f29ef2a95161a2ad2f0ad31f52e
rsninja722 <jnick722547@gmail.com>
2018-07-08 16:59:03 -0400

    null
    null

---
+++ null.js
@@ -0,0 +1,30 @@
+const null = {null null, null null, null null, null null, null null}
+var null = null;
+function null(null, null) {
+  while(null  null  null) {
+    null.splice(null,null,null);
+    console.log(Math.floor(Math.random(null)));
+    null[null]--;
+  }
+}
+for(var null = null; null  null; null++) {
+  null += null;
+  if(null !== null) {
+   null; 
+  } else {
+    null -= null;
+  }
+}
+null;
+let null = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
+if(null == null) {
+  null != null;
+} else if(null.null == null) {
+	null[null] = null;
+  null(null,null);
+}
+while(null) {
+  console.log(null);
+  null++;
+  null.push(null.toString());
+}
